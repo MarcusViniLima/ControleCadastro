fa40c836ff57b65e4b5b0ef87ad876d793b1f05b
MarcusViniLima <marcus12367lima@gmail.com>
2023-07-05 20:13:29 -0300

    Iniciando pelo Fornt-End

---
+++ ControleContatos/Controllers/ContatoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult adicionarContato()
+        {
+            return View();
+        }
+        public IActionResult editarContato()
+        {
+            return View();
+        }
+        public IActionResult apagarContatoConfirmacao()
+        {
+            return View();
+        }
+    }
+}
---
+++ ControleContatos/Controllers/HomeController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleContatos.Controllers
+{
+    public class ContatoController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
