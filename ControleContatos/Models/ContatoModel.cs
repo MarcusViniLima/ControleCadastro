4a15765e98b86a8b42bb1c1eeedcf97c419bb8f8
MarcusViniLima <marcus12367lima@gmail.com>
2023-07-12 20:14:44 -0300

    Inciando conexão com BD SQLServer

---
+++ ControleContatos/Models/ContatoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ControleContatos.Models
+{
+    public class ContatoModel
+    {
+        private int id {get; set;}
+        private string name { get; set;}
+        private string email { get; set;}
+        private string phone { get; set;}
+
+    }
+}
